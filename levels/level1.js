ace9396e37fa667ee4d634577ca5935221d143da
Julian W <Julian.wuenkhaus@gmx.net>
2024-07-10 14:42:58 +0200

    levels

--- levels/level1.js
+++ levels/level1.js
@@ -5,6 +5,11 @@ const level1 = new Level(
         new Enemy()
     ],
     [
+        new Sky('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/sky.png', 0, 0),
+        new Sky('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/sky.png', 720, 0),
+        new Sky('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/sky.png', 720 * 2, 0)
+    ],
+    [
         new BackgroundObject('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/graves.png', 0, 0),
         new BackgroundObject('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/back_trees.png', 0, 0),
         new BackgroundObject('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/tree.png', 0, 0),
@@ -24,10 +29,6 @@ const level1 = new Level(
         new BackgroundObject('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/wall.png', 720 * 2, 0),
         new BackgroundObject('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Pale/ground.png', 720 * 2, 0),
         new BackgroundObject('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/wall_mirrored.png', 720 * 2, 250)
-    ],
-    [
-        new Sky('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/sky.png', 0, 0),
-        new Sky('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/sky.png', 720, 0),
-        new Sky('../assets/Backgrounds/Battlegrounds/PNG/Graveyard Battleground4/Bright/sky.png', 720 * 2, 0)
     ]
+
 );
