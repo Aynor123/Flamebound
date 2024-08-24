effa98b90b914c89ecb2c40cfee1ac20d3b49d82
Julian W <Julian.wuenkhaus@gmx.net>
2024-08-24 21:25:47 +0200

    update enboss movement

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -9,6 +9,7 @@ class Endboss extends MoveableObject {
    tolerance = 3;
    castingTimeout = 0;
    isCasting = false;
+   animationInterval;
 
    IMAGES_IDLE = [
       '../assets/Enemies/witch/Witch_2/idle_2/tile000.png',
@@ -134,26 +135,61 @@ class Endboss extends MoveableObject {
          }
       }, 1000 / 10);
 
+      //    let castPoison = setInterval(() => {
+      //       let currentTime = Date.now(); // Get the current time in milliseconds
+
+      //       // Calculate the time difference in seconds since the last cast
+      //       let timeSinceLastCast = (currentTime - this.castingTimeout) / 1000;
+
+      //       // Check if the witch is in range and if 2 seconds have passed since the last cast
+      //       if (this.x - world.character.x <= world.rangeToRightFireball && timeSinceLastCast >= 2 && !this.isCasting) {
+      //          //  this.isCasting = true;
+      //           this.castingTimeout = currentTime; // Update castingTimeout to the current time
+
+      //           // Start the animation
+      //           this.playAnimation(this.IMAGES_CASTING); 
+
+      //           // Set a timeout to reset isCasting after the animation duration (0.5 seconds)
+      //           setTimeout(() => {
+      //               this.isCasting = false;
+      //           }, 500); // 500ms for 0.5 seconds
+      //       }
+      //   }, 100);
+
       let castPoison = setInterval(() => {
          let currentTime = Date.now(); // Get the current time in milliseconds
      
          // Calculate the time difference in seconds since the last cast
          let timeSinceLastCast = (currentTime - this.castingTimeout) / 1000;
      
-         // Check if the witch is in range and if 2 seconds have passed since the last cast
+         // Check if the witch is in range, 2 seconds have passed since last cast, and not currently casting
          if (this.x - world.character.x <= world.rangeToRightFireball && timeSinceLastCast >= 2 && !this.isCasting) {
-            //  this.isCasting = true;
-             this.castingTimeout = currentTime; // Update castingTimeout to the current time
-             
-             // Start the animation
-             this.playAnimation(this.IMAGES_CASTING); 
-     
-             // Set a timeout to reset isCasting after the animation duration (0.5 seconds)
-             setTimeout(() => {
-                 this.isCasting = false;
-             }, 500); // 500ms for 0.5 seconds
+             this.castingTimeout = currentTime; // Update the casting timeout to the current time
+             this.startCastingAnimation.call(this); // Start the casting animation
          }
      }, 100);
+
    }
+
+   startCastingAnimation() {
+      this.isCasting = true; // Set the flag to indicate casting is in progress
+  
+      // Start an interval to run the animation frames
+      this.animationInterval = setInterval(() => {
+          this.playAnimation(this.IMAGES_CASTING);
+  
+          // Calculate the time difference since the casting started
+          let currentTime = Date.now();
+          let animationDuration = (currentTime - this.castingTimeout) / 1000;
+  
+          // Stop the animation after 0.5 seconds (duration of the animation)
+          if (animationDuration >= 1.2) {
+              clearInterval(this.animationInterval); // Stop the animation interval
+              this.isCasting = false; // Reset casting flag
+          }
+      }, 1000 / 20); // Adjust interval based on frame count for smooth animation
+  }
+
+
 }
 
